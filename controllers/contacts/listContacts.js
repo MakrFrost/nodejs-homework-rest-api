064918a2d06d6116d9fe0228d2fb637fa6fbc8b7
MakrFrost <frostmark@ukr.net>
2023-02-05 20:51:51 +0200

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,13 +1,13 @@
 const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
-  const { _id: owner } = req.user;
+  const { _id } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt, -updatedAt", {
+  const result = await Contact.find({ owner: _id }, "-createdAt, -updatedAt", {
     skip,
     limit,
-  }).populate("owner", "name email");
+  }).populate("owner", "_id name email");
   res.json(result);
 };
 
