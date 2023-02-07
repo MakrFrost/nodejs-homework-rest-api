6133e708270ff7da22534931cccfb29a514873f0
MakrFrost <frostmark@ukr.net>
2023-02-07 15:38:45 +0200

    fixed auth

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -2,8 +2,12 @@ const { Contact } = require("../../models/contact");
 const { RequestError } = require("../../helpers");
 
 const removeContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await Contact.findByIdAndRemove(contactId);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.findByIdAndRemove({
+    _id: req.params.contactId,
+    owner,
+  });
   if (!result) {
     throw RequestError(404, "Not found");
   }
